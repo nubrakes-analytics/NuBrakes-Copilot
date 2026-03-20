0e584367d2342845bcaac8cd32fa3648932ff443
nubrakes-analytics <jonathan.libiran@nubrakes.com>
2026-03-20 19:49:40 +0800

    Update index.ts

--- worker/index.ts
+++ worker/index.ts
@@ -140,7 +140,8 @@ type AppApiResponse = {
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  "Access-Control-Max-Age": "86400",
 };
 
 const DASHBOARD_LINKS_URL =
